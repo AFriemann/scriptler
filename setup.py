495281016a7aa462d1643ef4a470a0320e15b341
Aljosha Friemann <aljosha.friemann@gmail.com>
2016-11-27 19:25:01 +0100

    setup will now use local package version and validate against git tag, which should take care of installed package not being versioned

--- setup.py
+++ setup.py
@@ -27,45 +27,35 @@ def sh(*args):
 def current_commit():
     return sh('git', 'rev-parse', '--short', 'HEAD')
 
-def last_git_tag():
-    return sh('git', 'describe', '--abbrev=0', '--tags')
-
 def git_tag_for(commit):
     return sh('git', 'tag', '--points-at', commit)
 
-def git_version():
+def check_version_against_tag():
     commit = current_commit()
     tag = git_tag_for(commit)
 
-    if not tag:
-        # this is not PEP440 compatible
-        return '{0}-{1}'.format(last_git_tag(), commit)
-
     if tag and tag != __version__:
         raise Exception('internal version {} is not equal to deployed version {}'.format(__version__, tag))
-
-    return tag
 
 install_reqs = pip.req.parse_requirements('requirements.txt', session=pip.download.PipSession())
 
 requirements = [str(ir.req) for ir in install_reqs if ir is not None]
 
-version = git_version()
+check_version_against_tag()
 
 setup(name             = "scriptler",
       author           = "Aljosha Friemann",
       author_email     = "aljosha.friemann@gmail.com",
       description      = "manage scripts from different sources",
+      url              = "https://www.github.com/afriemann/scriptler",
+      download_url     = "https://github.com/AFriemann/scriptler/archive/{}.tar.gz".format(__version__),
+      keywords         = ['scripts'],
+      version          = __version__,
       license          = read('LICENSE.txt'),
       long_description = read('README.rst'),
-      url              = "https://www.github.com/afriemann/scriptler",
-      download_url     = "https://github.com/AFriemann/scriptler/archive/{}.tar.gz".format(version),
-      keywords         = ['scripts'],
-      version          = version,
       install_requires = requirements,
       classifiers      = [],
       packages         = ["scriptler"],
-      package_data     = { 'scriptler': ['VERSION'] },
       entry_points     = { 'console_scripts': ['scriptler=scriptler.cli:run'] },
       platforms        = 'linux'
 )
