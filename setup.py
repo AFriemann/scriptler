209cb623efdafc57134ab83d55d3884ed858d984
Aljosha Friemann <a.friemann@automate.wtf>
2017-09-07 14:59:40 +0200

    version will be set by setup.py

--- setup.py
+++ setup.py
@@ -8,15 +8,13 @@
 
 """
 
-import os, pip, sys
+import os, sys
 from subprocess import Popen, PIPE
 
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
-    from distutils.core import setup
-
-from scriptler import __version__
+    from distutils.core import setup, find_packages
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname), 'r').read()
@@ -30,33 +28,49 @@ def current_commit():
 def git_tag_for(commit):
     return sh('git', 'tag', '--points-at', commit)
 
-def check_version_against_tag():
+def latest_tag():
+    return sh('git', 'describe', '--abbrev=0', '--tags')
+
+def get_version():
     commit = current_commit()
     tag = git_tag_for(commit)
 
-    if tag and tag != __version__:
-        raise Exception('internal version {} is not equal to deployed version {}'.format(__version__, tag))
+    return tag or '{}-{}'.format(latest_tag(), commit)
 
-install_reqs = pip.req.parse_requirements('requirements.txt', session=pip.download.PipSession())
+def set_version(version):
+    template = read('scriptler/__init__.py')
 
-requirements = [str(ir.req) for ir in install_reqs if ir is not None]
+    with open('scriptler/__init__.py', 'w') as output:
+        for line in template.split('\n'):
+            if 'VERSION' in line:
+                line = line.replace('<VERSION>', version)
+            output.write(line + os.linesep)
+
+VERSION = get_version()
 
 if 'upload' in sys.argv or 'register' in sys.argv:
-    check_version_against_tag()
+    set_version(VERSION)
 
 setup(name             = "scriptler",
       author           = "Aljosha Friemann",
       author_email     = "a.friemann@automate.wtf",
       description      = "manage scripts from different sources",
       url              = "https://www.github.com/afriemann/scriptler",
-      download_url     = "https://github.com/AFriemann/scriptler/archive/{}.tar.gz".format(__version__),
+      download_url     = "https://github.com/AFriemann/scriptler/archive/{}.tar.gz".format(VERSION),
       keywords         = ['scripts'],
-      version          = __version__,
+      version          = VERSION,
       license          = read('LICENSE.txt'),
       long_description = read('README.rst'),
-      install_requires = requirements,
+      install_requires = [
+            "click",
+            "simple_model>=1.1.2",
+            "simple_tools>=0.0.3",
+            "ruamel.yaml",
+            "requests",
+            "tabulate",
+      ],
       classifiers      = [],
-      packages         = ["scriptler"],
+      packages         = find_packages(exclude=('tests',)),
       entry_points     = { 'console_scripts': ['scriptler=scriptler.cli:run'] },
       platforms        = 'linux'
 )
